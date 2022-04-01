7f1de789ea6b3244143150ac7d3b0fc84962b256
Fernando Morejon Fausto <morejonff@gmail.com>
2022-04-01 12:56:28 -0600

    tarea1

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,23 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(const MyApp());
+//flutter run -t lib/exercise1.dart
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: '1st ITK Assignment',
-      home: Container(),
-    );
-  }
+void main() {
+  runApp(MaterialApp(
+    home: Scaffold(
+        backgroundColor: Colors.black,
+        body: SizedBox(
+          height: double.infinity,
+          width: double.infinity,
+          child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: const [
+                Text(
+                  "Hello Flutter!",
+                  style: TextStyle(color: Colors.white),
+                )
+              ]),
+        )),
+  ));
 }
