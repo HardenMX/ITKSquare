a30b99773df52e31f9c1152042246d7a7b5bc849
Fernando Morejon Fausto <morejonff@gmail.com>
2022-04-01 12:50:51 -0600

    main reset

--- lib/main.dart
+++ lib/main.dart
@@ -8,34 +8,9 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        debugShowCheckedModeBanner: false,
-        title: '1st ITK Assignment',
-        home: Scaffold(
-            appBar: AppBar(
-              title: const Text('Hola Sundaravel'),
-              centerTitle: true,
-            ),
-            body: Center(
-              child: Column(
-                children: [
-                  const Text('Salut Monde!',
-                      style: TextStyle(
-                        fontSize: 50,
-                        fontWeight: FontWeight.w700,
-                        color: Colors.deepPurple,
-                      )),
-                  Image.network('https://i.imgur.com/r8SVnnx.jpg')
-                ],
-              ),
-            ),
-            bottomNavigationBar: BottomNavigationBar(
-              items: const [
-                BottomNavigationBarItem(
-                    icon: Icon(Icons.settings), label: 'Settings'),
-                BottomNavigationBarItem(
-                    icon: Icon(Icons.catching_pokemon_outlined),
-                    label: 'Pokemon')
-              ],
-            )));
+      debugShowCheckedModeBanner: false,
+      title: '1st ITK Assignment',
+      home: Container(),
+    );
   }
 }
