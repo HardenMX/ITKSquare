2b67ca1f9357837c6407d38f8193b1a4bdadf3da
Fernando Morejon Fausto <morejonff@gmail.com>
2022-04-03 21:53:36 -0500

    03_display_circle_image

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,21 @@
 import 'package:flutter/material.dart';
 
-//flutter run -t lib/exercise2.dart
+//flutter run -t lib/exercise3.dart
 
 void main() {
   runApp(MaterialApp(
       home: Scaffold(
-    body: Center(
-        child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
+          body: Center(
+    child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
           SizedBox(
-              width: 50,
-              child: Image.network("assets/images/facebookicon.png")),
-        ])),
-  )));
+            width: 50,
+            child: ClipRRect(
+                borderRadius: BorderRadius.circular(50.0),
+                child: Image.network("https://i.imgur.com/sFcgDpo.png")),
+          )
+        ]),
+  ))));
 }
